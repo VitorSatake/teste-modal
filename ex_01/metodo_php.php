ae16f8f604f1e724ee76487d0f96e9a5c5c0f846
Vitor Satake <vitor.scastro2013@hotmail.com>
2023-11-06 18:50:38 -0300

    Enviando arquivos

--- ex_01/metodo_php.php
+++ ex_01/metodo_php.php
@@ -1,4 +1,12 @@
-
+<!--
+1) A ModalGR possui um cofre eletrônico que precisa estar protegido por 3 senhas, sendo 
+essas criptografadas e armazenadas numa base de dados SQL. Cada uma dessas senhas deve 
+possuir uma regra/método de criptografia distinta, mas ambas devem utilizar uma única 
+chave secreta. Para isso, você foi escolhido para criar o sistema de criptografia dessas senhas.
+Chave secreta:
+#modalGR#GPTW#top#maiorEmpresaTecnologia#baixadaSantista
+*/
+-->
 
 <?php
 // Chave secreta
--- ex_01/metodo_python.py
+++ ex_01/metodo_python.py
@@ -1,3 +1,15 @@
+'''
+1) A ModalGR possui um cofre eletrônico que precisa estar protegido por 3 senhas, sendo 
+essas criptografadas e armazenadas numa base de dados SQL. Cada uma dessas senhas deve 
+possuir uma regra/método de criptografia distinta, mas ambas devem utilizar uma única 
+chave secreta. Para isso, você foi escolhido para criar o sistema de criptografia dessas senhas.
+Chave secreta:
+#modalGR#GPTW#top#maiorEmpresaTecnologia#baixadaSantista
+
+'''
+
+
+
 import cryptocode
 
 # criptografando a senha 12345678 e passando a chave secreta para descriptografar
@@ -9,32 +21,3 @@ descriptografando = cryptocode.decrypt(criptografando, chave)
 print(descriptografando)
 
 
-
-
-
-
-
-
-
-
-
-
-'''
-import os
-from Cryptodome.Protocol.KDF import PBKDF2
-chave_secreta = b'#modalGR#GPTW#top#maiorEmpresaTecnologia#baixadaSantista'  # Substitua pela sua chave secreta
-salt = os.urandom(16)
-senha = "123456".encode()
-chave_derivada = PBKDF2(senha, salt, dkLen=32, count=100000)
-print(senha)
-
-
-
-
-
-import bcrypt
-chave_secreta = "#modalGR#GPTW#top#maiorEmpresaTecnologia#baixadaSantista"
-senha = "11111".encode()
-senha_hashed = bcrypt.hashpw(senha, chave_secreta)
-
-'''
